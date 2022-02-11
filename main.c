85def1e0bc6776edb2826452925ccb1c1a55f22f
Pierre Gagelin <pierre.gagelin@gmail.com>
2022-02-11 19:31:17 +0100

    Ajout d'un formulaire pour le déplacement d'une pièce

--- main.c
+++ main.c
@@ -10,13 +10,34 @@
 typedef struct _ContexteApp
 {
 	GtkWidget *label[4];
+
+	GtkWidget *deplacer_depuis;
+	GtkWidget *deplacer_vers;
 } ContexteApp;
+
+void gerer_validation(GtkWidget *objet, ContexteApp *ctx)
+{
+	// Paramètres non utilisés
+	(void)objet;
+	(void)ctx;
+
+	// Affiche un message avec les valeurs renseignées pour le déplacement
+	printf(
+		"L'utilisateur a validé son déplacement deplacer_depuis=%s deplacer_vers=%s\n",
+		gtk_entry_get_text(GTK_ENTRY(ctx->deplacer_depuis)),
+		gtk_entry_get_text(GTK_ENTRY(ctx->deplacer_vers)));
+
+	// Remet à zéro les entrées
+	gtk_entry_set_text(GTK_ENTRY(ctx->deplacer_depuis), "");
+	gtk_entry_set_text(GTK_ENTRY(ctx->deplacer_vers), "");
+}
 
 int main(int argc, char **argv)
 {
 	ContexteApp *ctx;
 	GtkWidget *fenetre;
 	GtkWidget *grille;
+	GtkWidget *objet;
 
 	gtk_init(&argc, &argv);
 
@@ -49,6 +70,23 @@ int main(int argc, char **argv)
 	ctx->label[3] = gtk_label_new(NOIR_ROI);
 	gtk_widget_set_name(GTK_WIDGET(ctx->label[3]), "case_gris");
 	gtk_grid_attach(GTK_GRID(grille), ctx->label[3], 1, 1, 1, 1);
+
+	// Crée une entrée dans laquelle l'utilisateur peut renseigner la position de la pièce à déplacer
+	ctx->deplacer_depuis = gtk_entry_new();
+	objet = gtk_label_new("pièce en");
+	gtk_grid_attach(GTK_GRID(grille), objet, 2, 0, 1, 1);
+	gtk_grid_attach(GTK_GRID(grille), ctx->deplacer_depuis, 3, 0, 1, 1);
+
+	// Crée une entrée dans laquelle l'utilisateur peut renseigner la position vers laquelle déplacer la pièce
+	ctx->deplacer_vers = gtk_entry_new();
+	objet = gtk_label_new("vers");
+	gtk_grid_attach(GTK_GRID(grille), objet, 2, 1, 1, 1);
+	gtk_grid_attach(GTK_GRID(grille), ctx->deplacer_vers, 3, 1, 1, 1);
+
+	objet = gtk_button_new();
+	gtk_button_set_label(GTK_BUTTON(objet), "Valider");
+	g_signal_connect(G_OBJECT(objet), "clicked", G_CALLBACK(gerer_validation), ctx);
+	gtk_grid_attach(GTK_GRID(grille), objet, 2, 2, 1, 2);
 
 	gtk_widget_show_all(fenetre);
 	gtk_main();
